0c47318c0e737be0af1e45d8090b8901263560e0
Memnoc <smartdroidesign@gmail.com>
2020-10-01 17:32:07 +0100

    new: state

--- React-basics/react-basics/src/App.js
+++ React-basics/react-basics/src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    person: [
+      { name: 'Odin', age: 11028},
+      { name: 'Thor', age: 3242},
+      { name: 'Hela', age: 5328}
+    ]
+  }
+
+
   render() {
     return (
       <div className="App">
         <h1>Hello I am a React app</h1>
-        <Person name = "Odin" age="11028" />
-        <Person name = "Thor" age="3234"> My hobbies: War and Parties </Person>
-        <Person name = "Hela" age="5328" />
+        <button>Switch names</button>
+        <Person name = {this.state.person[0].name} age={this.state.person[0].age} />
+        <Person name = {this.state.person[1].name} age={this.state.person[1].age}> My hobbies: War and Parties </Person>
+        <Person name = {this.state.person[2].name} age={this.state.person[2].age} />
       </div>
     );
   }
