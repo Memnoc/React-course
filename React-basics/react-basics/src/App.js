e1cdca8e4fe47359bc95ff3f40aab6df233975a4
Memnoc <smartdroidesign@gmail.com>
2020-10-05 17:21:24 +0100

    new: binding and styling

--- React-basics/react-basics/src/App.js
+++ React-basics/react-basics/src/App.js
@@ -37,10 +37,20 @@ class App extends Component {
   }
 
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
       <div className="App">
         <h1>Hello I am a React app</h1>
-        <button onClick={() => this.switchNameHandler('Odiiiiiinnnnn!')}>Switch names</button>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler('Odiiiiiinnnnn!')}>Switch names</button>
         <Person
          name = {this.state.person[0].name} age={this.state.person[0].age}  />
         <Person
--- React-basics/react-basics/src/Person/Person.js
+++ React-basics/react-basics/src/Person/Person.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import "./Person.css";
 
 // Functional component
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I am {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
             <input type="text" defaultValue={props.name} onChange={props.changed}/>
