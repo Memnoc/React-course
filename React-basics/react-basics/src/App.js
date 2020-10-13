dd59551bf36aa669baafdb4ae517b0aca40bfb7a
Memnoc <smartdroidesign@gmail.com>
2020-10-13 10:16:47 +0100

    new: radium and pseudo selectors

--- React-basics/react-basics/src/App.js
+++ React-basics/react-basics/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-
+import Radium from 'radium';
 
 class App extends Component {
   state = {
@@ -59,7 +59,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
   let persons = null;
@@ -79,6 +83,10 @@ class App extends Component {
     );
 
     style.backgroundColor = 'red';
+    style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
   }
 
   const classes = [];
@@ -102,7 +110,7 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
 
 
 
