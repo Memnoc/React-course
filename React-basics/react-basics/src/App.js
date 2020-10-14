db50c72e0bea170c25a6ffa4e625274333827f02
Memnoc <smartdroidesign@gmail.com>
2020-10-14 10:27:08 +0100

    new: styled components

--- React-basics/react-basics/src/App.js
+++ React-basics/react-basics/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium, { StyleRoot } from 'radium';
-
 class App extends Component {
   state = {
     persons: [
@@ -98,7 +96,6 @@ class App extends Component {
   }
 
     return (
-      <StyleRoot>
       <div className="App">
         <h1>Hello I am a React app</h1>
         <p className={classes.join(' ')}>This is really working</p>
@@ -107,12 +104,11 @@ class App extends Component {
         onClick={this.togglePersonHandler}>Toggle Persons</button>
         {persons}
       </div>
-      </StyleRoot>
     );
   }
 }
 
-export default Radium(App);
+export default App;
 
 
 
